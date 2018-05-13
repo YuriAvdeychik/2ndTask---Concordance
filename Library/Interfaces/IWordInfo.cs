b1ce1e33a6554a33fdac011b857cc0a8509767ed
Yuri Avdeychik <yuri.avdeychik@gmail.com>
2018-05-13 17:05:12 +0300

    interfaces initialize

---
+++ Library/Interfaces/IWordInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Interfaces
+{
+    interface IWordInfo
+    {
+        string Value { get; }
+        int Count { get; }
+        void CountUp();
+        ICollection<int> PageNumbers { get; }
+        string PageNumbersToString();
+        void AddPageNumber(int pageNumber);
+    }
+}
