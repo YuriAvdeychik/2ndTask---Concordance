22da487cc562477491f0db6074ceee532740e3f3
Yuri Avdeychik <yuri.avdeychik@gmail.com>
2018-05-13 17:53:08 +0300

    create word info

--- Library/Classes/WordInfo.cs
+++ Library/Classes/WordInfo.cs
@@ -8,5 +8,54 @@ namespace Library.Classes
 {
     class WordInfo
     {
+        private int _count;
+        private string _value;
+        ICollection<int> _pageNumbers;
+
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+        }
+
+        public ICollection<int> PageNumbers
+        {
+            get
+            {
+                return _pageNumbers;
+            }
+        }
+        public string Value
+        {
+            get
+            {
+                return _value;
+            }
+        }
+
+        public WordInfo(string value, int pageNumber)
+        {
+            _value = value;
+            _count = 1;
+            _pageNumbers = new List<int>();
+            _pageNumbers.Add(pageNumber);
+        }
+
+        public void CountUp()
+        {
+            _count++;
+        }
+
+        public void AddPageNumber(int pageNumber)
+        {
+            if (_pageNumbers.Contains(pageNumber) == false)
+                _pageNumbers.Add(pageNumber);
+        }
+        public string PageNumbersToString()
+        {
+            return string.Join(" ", _pageNumbers);
+        }
     }
 }
